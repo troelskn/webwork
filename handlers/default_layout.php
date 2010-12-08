730bbd7cb003555fa3581049508be302a3f4e640
Troels Knak-Nielsen <troelskn@gmail.com>
2010-12-08 10:12:57 +0100

    Just fiddling with the layout

--- handlers/default_layout.php
+++ handlers/default_layout.php
@@ -4,7 +4,7 @@
   </head>
   <body>
 
- <?php echo $content; ?>
+<?php echo $content; ?>
 
   </body>
 </html>
--- handlers/root.php
+++ handlers/root.php
@@ -1,7 +1,7 @@
 <?php
 set_title("It Works");
 ?>
-<h1>It Works</h1>
+<h1>Welcome To Your Webwork Application</h1>
 <p>
   This file can be found in: <code style="background-color:lightblue"><?php e(__FILE__); ?></code>
 </p>
